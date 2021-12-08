6739e3c60b62847a522ffc62f16551832a4294c0
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2021-12-09 00:11:12 +0600

    invation package isnstall and post add er kaj baki ace

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Product;
+use Image;
+use Carbon\Carbon;
 
 class ProductController extends Controller
 {
@@ -33,7 +35,59 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        // return response()->json([
+        //     'status' => 'success',
+        //     'data' => $request->all()
+        //    ]);
+
+         $this->validate($request, [
+            'product_name' => 'required',
+            'category_id' => 'required',
+            'product_short_description' => 'required',
+            'product_long_description' => 'required',
+           'product_price'=>'required|integer',
+            'publication_status' => 'required',
+            'product_image' => 'required',
+
+        ]);
+
+         // data:image/jpeg;base64,/9j/4SUqRXhpZgAASUkqAA
+         // "data:image/jpeg"
+
+         $strpos = strpos($request->product_image, ';');
+         $string = substr($request->product_image, 0,$strpos);
+
+         $extention = substr(strrchr($string, '/'), 1);
+
+        $image_name = rand().".".$extention;
+        $image_save = public_path('images/products/'.$image_name);
+
+        // open an image file
+        $img = Image::make($request->product_image);
+        // now you are able to resize the instance
+        $img->resize(320, 240);
+        // finally we save the image as a new file
+        $img->save($image_save);
+
+
+        $insert_product =  Product::Create([
+           'product_name'=>$request->product_name,
+           'category_id'=>$request->category_id,
+           'product_short_description'=>$request->product_short_description,
+           'product_long_description'=>$request->product_long_description,
+           'product_price'=>$request->product_price,
+           'publication_status'=>$request->publication_status,
+           "product_image" => $image_name,
+           'created_at'=>Carbon::now(),
+        ]);
+
+          return response()->json([
+            'status' => 'success',
+            
+           ]);
+
+
     }
 
     /**
